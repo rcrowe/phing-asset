05e6014b02ab02ca65a59e4f9f25ccc3bedd1634
Rob Crowe <nobby.crowe@gmail.com>
2010-10-31 19:37:06 +0000

    Latest

--- lib/Asset.php
+++ lib/Asset.php
@@ -3,11 +3,12 @@
 class Asset extends DataType
 {
     private $assetsDir;
+    private $assetFolder;
     private $url;
-    
-    public $type;
-    public $file;
-    public $returnProperty;
+    private $paths;
+    private $type;
+    private $file;
+    private $returnProperty;
     
     //Constants
     const ASSET_TYPE_CSS   = 0;
@@ -15,11 +16,7 @@ class Asset extends DataType
     const ASSET_TYPE_IMAGE = 2;
     const ASSET_TYPE_OTHER = 3;
     
-    public function __construct($dir, $url)
-    {
-        $this->assetsDir = $dir;
-        $this->url       = $url;
-    }
+    public function __construct(){}
     
     public function setJs($file)
     {
@@ -48,6 +45,79 @@ class Asset extends DataType
     public function setReturnProperty($prop)
     {
         $this->returnProperty = $prop;
+    }
+    
+    public function main($assetDir, $url, $paths)
+    {
+        $this->assetsDir = $assetDir;
+        $this->url       = $url;
+        $this->paths     = $paths;
+        
+        if(empty($this->assetsDir))
+        {
+            throw new BuildException("Make sure you set the path to your assets", $this->location);
+        }
+        
+        if(empty($this->url))
+        {
+            throw new BuildException("Make sure you set the URL to your assets", $this->location);
+        }
+        
+        
+        $asset_file = $this->checkFileExists();
+        
+        $generated_url  = $this->generateURL($asset_file);
+        
+        //Set the generated URL to use in the buildfile
+        $this->project->setProperty($this->returnProperty, $generated_url);
+    }
+    
+    public function checkFileExists()
+    {
+        //Get the correct path to asset
+        switch($this->type)
+        {
+            case Asset::ASSET_TYPE_CSS:         $this->assetFolder = $this->paths['css'];
+                                                break;
+                                            
+            case Asset::ASSET_TYPE_JS:          $this->assetFolder = $this->paths['js'];
+                                                break;
+                                            
+            case Asset::ASSET_TYPE_IMAGE:       $this->assetFolder = $this->paths['images'];
+                                                break;
+
+            default:                            $folder = '';
+        }
+        
+        //Path to file
+        $file = new PhingFile($this->assetsDir.'/'.$this->assetFolder.$this->file);
+        
+        //Check file exists
+        if(!$file->exists())
+        {
+            throw new BuildException("Unable to find asset file: ".$file->getAbsolutePath());
+        }
+        
+        //Check we can read it
+        if(!$file->canRead())
+        {
+            throw IOException("Unable to read asset file: ".$file->getPath());
+        }
+        
+        return $file;
+    }
+    
+    public function generateURL(PhingFile $file)
+    {
+        //Build modified param to help with caching
+        $modtime   = $file->lastModified();
+        $mod_param = ($modtime !== 0) ? "?$modtime" : '';
+        
+        //Build path to actual file
+        $folder = (!empty($this->assetFolder)) ? $this->assetFolder : '';
+        $url_path = $folder.$this->file;
+        
+        return sprintf("http://%s%s%s", $this->url, $url_path, $mod_param);
     }
 }
 
--- lib/Assets.php
+++ lib/Assets.php
@@ -69,98 +69,14 @@ class Assets extends Task
             }
         }
         
-        /*
         //Handle building URLs to assets
         if(!empty($this->assets))
         {
             foreach($this->assets as $asset)
             {
-                $asset->main();
+                $asset->main($this->assetDir, $this->url, $this->paths);
             }
         }
-        
-        
-        if(empty($this->assetsDir))
-        {
-            throw new BuildException("Make sure you set the path to your assets", $this->location);
-        }
-        
-        if(empty($this->url))
-        {
-            throw new BuildException("Make sure you set the URL to your assets", $this->location);
-        }
-        
-        //Do we want to compress any files
-        if(!empty($this->compress))
-        {
-            foreach($this->compress as $compress)
-            {
-                //Handle JS compression
-                foreach($compress->compress_js as $js)
-                {
-                    
-                }
-            }
-        }
-        
-        //Generate URL for each file
-        foreach($this->assets as $asset)
-        {
-            $file = $this->checkFileExists($asset);
-            
-            $url  = $this->generateURL($asset, $file);
-            
-            //Set the generated URL to use in the buildfile
-            $this->project->setProperty($asset->returnProperty, $url);
-        }*/
-    }
-    
-    public function checkFileExists(Asset $asset)
-    {
-        //Get the correct path to asset
-        switch($asset->type)
-        {
-            case Asset::ASSET_TYPE_CSS:         $this->assetFolder = $this->paths['css'];
-                                                break;
-                                            
-            case Asset::ASSET_TYPE_JS:          $this->assetFolder = $this->paths['js'];
-                                                break;
-                                            
-            case Asset::ASSET_TYPE_IMAGE:       $this->assetFolder = $this->paths['images'];
-                                                break;
-
-            default:                            $this->assetFolder = '';
-        }
-        
-        //Path to file
-        $file = new PhingFile($this->assetsDir.'/'.$this->assetFolder.'/'.$asset->file);
-        
-        //Check file exists
-        if(!$file->exists())
-        {
-            throw new BuildException("Unable to find asset file: ".$file->getAbsolutePath());
-        }
-        
-        //Check we can read it
-        if(!$file->canRead())
-        {
-            throw IOException("Unable to read asset file: ".$file->getPath());
-        }
-        
-        return $file;
-    }
-    
-    public function generateURL(Asset $asset, PhingFile $file)
-    {
-        //Build modified param to help with caching
-        $modtime   = $file->lastModified();
-        $mod_param = ($modtime !== 0) ? "?$modtime" : '';
-        
-        //Build path to actual file
-        $folder = (!empty($this->assetFolder)) ? $this->assetFolder : '';
-        $url_path = $folder.$asset->file;
-        
-        return sprintf("http://%s%s%s", $this->url, $url_path, $mod_param);
     }
 }
 
